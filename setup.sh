3a81443834b237c8d0c1f7df01f46db069bbfdf5
potatowagon <th3bomberry@hotmail.com>
2018-12-20 22:27:15 +0800

    save git cred

--- setup.sh
+++ setup.sh
@@ -3,4 +3,7 @@ sudo apt-get upgrade
 sudo apt-get install git
 sudo apt-get install git-lfs
 chmod +x ./setup_git_lfs.sh
+chmod +x ./dhusget.sh
+git config credential.helper store
 ./setup_git_lfs.sh
+touch secrets.py
