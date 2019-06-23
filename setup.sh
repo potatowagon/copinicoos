470af6e05c88d11b51af332930732730672efb57
potatowagon <th3bomberry@hotmail.com>
2019-06-23 11:54:26 +0800

    cleanup unused files

--- setup.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git
-sudo apt-get install git-lfs
-chmod +x ./setup_git_lfs.sh
-chmod +x ./dhusget.sh
-git config credential.helper store
-./setup_git_lfs.sh
-touch secrets.py
--- setup_git_lfs.sh
+++
@@ -1,6 +0,0 @@
-git lfs install
-git lfs track '*.zip'
-git add .gitattributes "*.zip"
-git commit -m "add gitattributes for lfs"
-git lfs migrate import --include="*.zip"
-git push
