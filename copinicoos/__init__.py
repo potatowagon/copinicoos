9b38c21fb322b0fd3c40bb8ba3ce7ef0f8bb23ae
potatowagon <th3bomberry@hotmail.com>
2019-06-23 11:45:53 +0800

    wip

--- copinicoos/__init__.py
+++ copinicoos/__init__.py
@@ -1,3 +1,31 @@
+"""
+The Copinicoos library opens up the API for downloading Satellite images from Copernicus Hub
+"""
+
 import colorama
 
+from .input_manager import InputManager as _InputManager
+from .worker import Worker as _Worker
+from .worker_manager import WorkerManager as _WorkerManager
+
+__version__ = "0.0.1"
+
+#__all__ = ["login"]
+
 colorama.init(convert=True, autoreset=True)
+
+'''
+def login(**creds):
+    """ Input copernicus account credentials to initialise a WorkerManager.
+
+    Input credentials as such, for example:
+
+    ``login(u1="username1", p1="password1", u2="username2", p2="password2")``
+
+    will create 2 Workers and return the Workers in a list. The worker list is used to initialise WorkerManager.
+
+    returns initialised WorkerManager class
+    """
+
+    input_manager = 
+'''
