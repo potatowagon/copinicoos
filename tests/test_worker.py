308a2599e1fa2f13558adbe0f65fb5711a998a96
potatowagon <th3bomberry@hotmail.com>
2019-07-14 01:42:53 +0800

    query_product_uri passed

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -8,4 +8,11 @@ import pytest
     ]
 )
 def test_query_product_uri_success(worker, result_num):
-    worker.query_product_uri(result_num)
+    title, product_uri = worker.query_product_uri(result_num)
+    try:
+        assert title.startswith("S1A") == True
+        assert product_uri.startswith('"https://scihub.copernicus.eu/dhus/odata/v1/Products(') == True
+        assert product_uri.endswith('/$value"') == True
+    except Exception as e:
+        print(title, product_uri)
+        raise
