44177cfde1dee25b05a2393d60a591a6913cf5ec
potatowagon <th3bomberry@hotmail.com>
2019-08-10 12:06:06 +0800

    make Worker and AccountManager public for API testing

--- copinicoos/__init__.py
+++ copinicoos/__init__.py
@@ -5,8 +5,9 @@ The Copinicoos library opens up the API for downloading Satellite images from Co
 import colorama
 
 from .input_manager import InputManager
-from .worker import Worker as _Worker
+from .worker import Worker as Worker
 from .worker_manager import WorkerManager 
+from .account_manager import AccountManager
 
 __version__ = "0.0.1"
 
