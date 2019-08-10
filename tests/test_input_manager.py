6936d5aa104c6b02ea6610adb1bc06db2fdf1178
potatowagon <th3bomberry@hotmail.com>
2019-08-10 13:12:02 +0800

    regression test passed for account manager refractor

--- tests/test_input_manager.py
+++ tests/test_input_manager.py
@@ -6,31 +6,8 @@ import pytest
 from copinicoos import InputManager
 from conftest import test_data_dir, test_dir
 
-def test_add_worker_success(creds):
-    im = InputManager()
-    assert im.add_worker(creds["u1"], creds["p1"]) == True
-    assert len(im.worker_list) == 1
-    assert type(im.worker_list[0]).__name__ == "Worker"
-
-def test_add_worker_wrong_creds():
-    im = InputManager()
-    assert im.add_worker("nada", "zip") == False
-    assert len(im.worker_list) == 0
-
-def test_multi_add_worker_success(creds):
-    im = InputManager()
-    assert im.add_worker(creds["u1"], creds["p1"]) == True
-    assert im.add_worker(creds["u2"], creds["p2"]) == True
-    assert len(im.worker_list) == 2
-
-def test_multi_add_worker_repeat_creds(creds):
-    im = InputManager()
-    assert im.add_worker(creds["u1"], creds["p1"]) == True
-    assert im.add_worker(creds["u1"], creds["p1"]) == False
-    assert len(im.worker_list) == 1
-
-def test_get_total_results_from_query_success(query, input_manager_with_one_worker):
-    im = input_manager_with_one_worker
+def test_get_total_results_from_query_success(query, input_manager_with_2_workers):
+    im = input_manager_with_2_workers
     tr = im.get_total_results_from_query(query)
     print(str(tr))
     assert type(im.args.total_results) is int
@@ -62,16 +39,3 @@ def test_get_json_creds_badfile(arg):
     with pytest.raises(Exception) as e:
         im.get_json_creds(arg)
     assert "No such file or directory" in str(e.value)
-
-@pytest.mark.parametrize(
-    "json, expected_workers", [
-        (json.load(open(os.path.join(test_data_dir,'secrets1.json'))), 1),
-        (json.load(open(os.path.join(test_data_dir,'secrets2.json'))), 2)
-    ]
-)
-def test_add_workers_from_json_creds(json, expected_workers):
-    im = InputManager()
-    im.add_workers_from_json_creds(json)
-    assert len(im.worker_list) == expected_workers
-    for worker in im.worker_list:
-        assert type(worker).__name__ == "Worker"
