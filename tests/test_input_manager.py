ee949389e7a270b29c66c6a55269bf043649aa6f
potatowagon <th3bomberry@hotmail.com>
2019-08-10 23:34:41 +0800

    test interactive input passed

--- tests/test_input_manager.py
+++ tests/test_input_manager.py
@@ -4,6 +4,7 @@ import os
 import pytest
 
 from copinicoos import InputManager
+from copinicoos import input_manager
 from conftest import query_txt_path, secrets1_json_path, secrets2_json_path
 
 @pytest.mark.parametrize(
@@ -52,3 +53,40 @@ def test_get_json_creds_badfile(arg):
     with pytest.raises(Exception) as e:
         im.get_json_creds(arg)
     assert "No such file or directory" in str(e.value)
+
+def test_interactive_input(capsys, creds, query):
+    input_values = [
+        2,
+        creds["u1"],
+        creds["p1"],
+        creds["u2"],
+        creds["p2"],
+        query,
+        "\n",
+        "\n",
+        "\n"
+    ]
+    im = InputManager()
+    def mock_input():
+        return input_values.pop(0)
+    input_manager.input = mock_input
+    im.interactive_input()
+    out, err = capsys.readouterr()
+    assert "Enter number of accounts:" in out
+    assert "Enter username of account" in out
+    assert "Enter password of account" in out
+    assert "Authenticating worker..." in out
+    assert "Worker sucessfully authenticated." in out
+    assert "Enter query:" in out
+    assert "products found" in out
+    assert "Default download directory set to" in out
+    assert "Enter new path" in out
+    assert "Default polling interval" in out
+    assert "Enter new polling interval" in out
+    assert "Default offline retries" in out
+    assert "Enter new offline retries" in out
+
+    args = im.return_args() 
+    assert type(args).__name__ == "Args"
+    assert len(im.return_worker_list()) == 4
+    
