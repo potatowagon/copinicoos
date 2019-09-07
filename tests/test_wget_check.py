51a3df176802029a555af621eec78d96218968a8
potatowagon <th3bomberry@hotmail.com>
2019-09-07 21:01:00 +0800

    debug failing ci test on non windows machines

--- tests/test_wget_check.py
+++ tests/test_wget_check.py
@@ -6,7 +6,7 @@ def test_is_wget_installed():
     assert wget_check.is_wget_installed()
 
 def test_is_choco_installed():
-    assert wget_check.is_choco_installed()
+    assert wget_check.is_choco_installed() == wget_check.is_windows()
 
 def test_install_choco():
     if wget_check.is_windows():
