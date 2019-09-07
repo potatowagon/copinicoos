275d8630df21a47b570e0964f322f7d3cbd78698
potatowagon <th3bomberry@hotmail.com>
2019-09-07 18:14:17 +0800

    install wget through python

---
+++ copinicoos/wget_check.py
@@ -0,0 +1,51 @@
+import subprocess
+import platform
+
+from colorama import Fore
+
+def is_wget_installed() -> bool:
+    '''Check if wget is installed by calling it through a subprocess and analysing the output
+
+    Returns:
+        is_installed (bool)
+    '''
+    cmd = ["wget", "--version"]
+    out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    is_installed = 'Hrvoje Niksic' in str(out)
+    return is_installed
+
+def is_macos() -> bool:
+    system = platform.system()
+    return 'Darwin' in system
+
+def is_windows() -> bool:
+    system = platform.system()
+    return 'Windows' in system
+
+def is_linux() -> bool:
+    system = platform.system()
+    return 'Linux' in system
+
+def is_choco_installed() -> bool:
+    cmd = ["choco", "-?"]
+    out = str(subprocess.check_output(cmd, stderr=subprocess.STDOUT))
+    return 'Chocolatey' in out
+
+def install_choco() -> bool:
+    print(Fore.YELLOW + "Attempting to install chocolatey. This step requires admin rights. If this fails, try running copinicoos from an Administrative shell, or install chocolatey yourself.")
+    cmd = '@\"%SystemRoot%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe\" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command \"iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'))\" && SET \"PATH=%PATH%;%ALLUSERSPROFILE%\\chocolatey\\bin'
+    subprocess.call(cmd, shell=True)
+
+def install_wget():
+    print(Fore.YELLOW + "Copinicoos uses wget to download, and shall now attempt to install wget. If installation fails, please install wget yourself.")
+    if is_windows():
+        if not is_choco_installed():
+            install_choco()
+        cmd = ["choco", "install", "wget"]
+        subprocess.call(cmd)
+    elif is_macos():
+        cmd = ["brew", "install", "wget"]
+        subprocess.call(cmd)
+    elif is_linux():
+        cmd = ["apt-get", "install", "wget"]
+        subprocess.call(cmd)
---
+++ tests/test_wget_check.py
@@ -0,0 +1,16 @@
+import pytest
+
+from copinicoos import wget_check
+
+def test_is_wget_installed():
+    assert wget_check.is_wget_installed()
+
+def test_is_choco_installed():
+    assert wget_check.is_choco_installed()
+
+def test_install_choco():
+    if wget_check.is_windows():
+        wget_check.install_choco()
+
+def test_install_wget():
+    wget_check.install_wget()
